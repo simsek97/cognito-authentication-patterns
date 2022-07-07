345ce8f00b4306fe9e072f8dc4b4f0c5a37c3f2b
sunchloe1 <sunchloe@amazon.comm>
2022-07-07 13:25:10 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import awsconfig from './aws-exports';
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
 function App({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user.username}, welcome to Chloe's web app!</h1>
+      <h1>Hello {user.username}, excited to welcome to Chloe's web app!</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
