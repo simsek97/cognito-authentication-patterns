22fc6cf25eb2e481b4108196ea00ff58a82b7009
sunchloe1 <sunchloe@amazon.comm>
2022-07-07 15:53:16 -0400

    Co-authored-by: Simsek Mert <simsek97@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator, CheckboxField } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const App = () => {
+  return (
+    <Authenticator
+      initialState="signUp"
+      components={{
+        SignUp: {
+          FormFields() {
+            const { validationErrors } = useAuthenticator();
+
+            return (
+              <>
+                {/* Re-use default `Authenticator.SignUp.FormFields` */}
+                <Authenticator.SignUp.FormFields />
+
+                {/* Append & require Terms & Conditions field to sign up  */}
+                <CheckboxField
+                  errorMessage={validationErrors.acknowledgement as string}
+                  hasError={!!validationErrors.acknowledgement}
+                  name="acknowledgement"
+                  value="yes"
+                  label="I agree with the Terms & Conditions"
+                />
+              </>
+            );
+          },
+        },
+      }}
+      services={{
+        async validateCustomSignUp(formData) {
+          if (!formData.acknowledgement) {
+            return {
+              acknowledgement: 'You must agree to the Terms & Conditions',
+            };
+          }
+        },
+      }}
+    >
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export default App;
