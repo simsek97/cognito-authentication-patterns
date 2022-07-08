879c6a95b5ebfa6741fa6b642648feb2ac084070
sunchloe1 <sunchloe@amazon.comm>
2022-07-08 09:47:38 -0400

    firstcom

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
                   hasError={!!validationErrors.acknowledgement}
                   name="acknowledgement"
                   value="yes"
-                  label="I agree with the Terms & Conditions."
+                  label="I agree with the Terms & Conditions!"
                 />
               </>
             );
